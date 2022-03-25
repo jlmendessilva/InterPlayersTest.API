d8931eb5d8f01461a87000631a5303259ab0c44e
JOAO LUIS MENDES DA SILVA BRITO <jlmendessilva@gmail.com>
2022-03-25 10:34:34 -0300

    Criacao com Entity User

---
+++ InterPlayersTest.Infra/Entitties/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InterPlayersTest.Infra.Entitties
+{
+    public class User
+    {
+        public User(string name, string password)
+        {
+            Id = new Guid();
+            Name = name;
+            Password = password;
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Password { get; private set; }
+    }
+}
